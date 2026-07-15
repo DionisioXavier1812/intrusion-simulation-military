7d45be9216186a48f50b80ae6fa3a475c01e29cb
Dionisio <dionisioxavier1812@gmail.com>
2026-07-14 23:32:18 -0300

    Simulação de ransomware militar adicionada ao Projeto 3

---
+++ scripts/detect-ransom.cmd
@@ -0,0 +1,4 @@
+findstr /i "mil_ransom.exe" ..\logs\ransomware.log 
+findstr /i "encrypted" ..\logs\ransomware.log 
+findstr /i "Ransom note" ..\logs\ransomware.log 
+findstr /i "Outbound" ..\logs\ransomware.log 
