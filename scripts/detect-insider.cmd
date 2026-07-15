2eff30c0623203b6b2244aa724e499770e0fdc9e
Dionisio <dionisioxavier1812@gmail.com>
2026-07-14 23:34:11 -0300

    Modulo de Insider Threat militar adicionado ao Projeto 3

---
+++ scripts/detect-insider.cmd
@@ -0,0 +1,5 @@
+findstr /i "oficial_ops" ..\logs\insider.log 
+findstr /i "restricted" ..\logs\insider.log 
+findstr /i "Policy violation" ..\logs\insider.log 
+findstr /i "usb" ..\logs\insider.log 
+findstr /i "203.0.113.50" ..\logs\insider.log 
